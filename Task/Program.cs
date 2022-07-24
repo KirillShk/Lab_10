e99d7617ce9dd8baa7a643655e967d505e1920c2
Kirill Shlyakov <Kirill Shlyakov@DESKTOP-4PP6PL7>
2022-07-24 22:07:00 +0300

    Добавьте файлы проекта.

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите поочередно значения в грудусах, минутах и секундах");
+            int gradus = Convert.ToInt32(Console.ReadLine());  
+            int minute = Convert.ToInt32(Console.ReadLine());  
+            int second = Convert.ToInt32(Console.ReadLine());
+            Radians radians = new Radians(gradus, minute, second);
+            double ResultRadians = radians.ToRadians();
+            Console.WriteLine("Значение в радианах = {0}", ResultRadians);
+            Console.ReadKey();
+        }
+    }
+}
